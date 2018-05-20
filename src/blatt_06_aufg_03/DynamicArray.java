d639d1b6e246de356f6f6726680817bce4fbb835
norman <norman@x250.fritz.box>
2018-05-20 02:04:44 +0200

    index fixes

--- src/blatt_06_aufg_03/DynamicArray.java
+++ src/blatt_06_aufg_03/DynamicArray.java
@@ -12,23 +12,24 @@ public class DynamicArray {
 	{
 		m = size;
 		n = 0;
-		a = new Item[m];
+		a = new Item[m+1]; //1-Indizierung
 	}
 	
 	public void setElement(int index, Item item) {
-		if(index > m-1) return;
+		if(index > m) return;
 		a[index] = item;
 	}
 	
 	public Item getElement(int index) {
-		if(index > m-1) return null;
+		if(index > m) return null;
 		return a[index];
 	}
 	
 	public void insert(Item item) {
-		if(n==0) {a[n] = item;return;}
-		if(n==m-1) {
-			Item[] b = new Item[2*m];
+		if(n==0) {a[++n] = item;return;} //1-Indizierung, a[0] ist leer
+		if(n==m) {
+			//System.out.println("n:" + n +" m: " + m);
+			Item[] b = new Item[2*m+1];
 			for (int i = 0; i < a.length; i++) {
 				b[i] = a[i];
 			}
@@ -40,11 +41,11 @@ public class DynamicArray {
 	}
 	
 	public void delete() {
-		a[m-1] = null;
+		a[n--] = null;
 	}
 	
 	public void print() {
-		for(int i = 0; i < m;i++) {
+		for(int i = 0; i <= m;i++) {
 			if(a[i] == null) System.out.print("null, ");
 			else System.out.print(a[i].getVal() + ", ");
 		}
@@ -55,12 +56,28 @@ public class DynamicArray {
 		DynamicArray a = new DynamicArray(1);
 		Item<Integer> i = new Item<>(5,5);
 		Item<Integer> j = new Item<>(3,6);
+		Item<Integer> k = new Item<>(3,7);
+		Item<Integer> l = new Item<>(3,123);
+		Item<Integer> m = new Item<>(3,9);
+
+
+
 		a.print();
 		a.insert(i);
 		a.print();
 		a.insert(j);
 		a.print();
-
+		a.insert(k);
+		a.print();
+		a.insert(l);
+		a.print();
+		a.insert(m);
+		a.print();
+		a.setElement(2, new Item<Integer>(3,134));
+		a.print();
+		System.out.println(a.getElement(4).getVal());
+		a.delete();
+		a.print();
 	}
 }
 
