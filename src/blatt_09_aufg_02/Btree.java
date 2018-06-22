cb4b669ae68718b56f45214fcae3ae676456a430
norman <norman@x250>
2018-06-22 17:33:32 +0200

    finished. fixed index arithmetic (i-(medianIndex+1))
    and update bro's children's parent pointer to "bro"

--- src/blatt_09_aufg_02/Btree.java
+++ src/blatt_09_aufg_02/Btree.java
@@ -8,11 +8,11 @@ public class Btree {
 	// Sie können den zusätzlichen Platz nutzen, um temporär einen Schlüssel
 	// "zu viel" in den Knoten einzufügen, bevor er geteilt wird.
 	private static class Node {
-		Node parent; // Vaterknoten.
+		Node parent = null; // Vaterknoten.
 		int[] keys = new int[4];
 		Node[] links = new Node[5];
-		boolean isLeaf; // Gibt an, ob der Knoten ein Blatt ist.
-		int count; // Aktuelle Anzahl Schlüssel (nicht Links!).
+		boolean isLeaf = false; // Gibt an, ob der Knoten ein Blatt ist.
+		int count=0; // Aktuelle Anzahl Schlüssel (nicht Links!).
 	}
 
 	// Überprüft, ob ein gegebener Schlüssel im Baum enthalten ist.
@@ -36,7 +36,7 @@ public class Btree {
 		}
 	}
 
-	public void insert(int key) {
+	public void insert2(int key) {
 		// Implementieren Sie hier den Algorithmus zum Einfügen.
 		// Beachten Sie, dass Sie ggf. eine neue Wurzel (Variable root) erzeugen
 		// müssen und dass root auf null initialisiert wird.
@@ -59,6 +59,7 @@ public class Btree {
 		
 		//insert in node
 		Node bro = null,bak=null;
+		boolean insertNode = true;
 		while(true) {
 			//create new root
 			if(insert == null) {
@@ -80,6 +81,7 @@ public class Btree {
 		for(int j = insert.count-1 ;j >= insertPos ;j--) {
 			insert.keys[j+1] = insert.keys[j];
 			insert.links[j+2] = insert.links[j+1];
+
 		}
 		insert.keys[insertPos] = key;
 		if(bro != null) insert.links[insertPos+1] = bro;
@@ -109,15 +111,124 @@ public class Btree {
 		bak = insert;
 		insert = insert.parent;
 		key = median;
-		for(int i = 0; i < bro.links.length;i++) {
+		/*for(int i = 0; i < bro.links.length;i++) {
 			if(bro.links[i] != null) {bro.isLeaf = false;break;}
-		}
+		}*/
+		//if(insertNode) {bro.isLeaf = true;}
+		
 		}
 		
 
 		
 	}
 
+	public void insert(int key) {
+		// Implementieren Sie hier den Algorithmus zum Einfügen.
+		// Beachten Sie, dass Sie ggf. eine neue Wurzel (Variable root) erzeugen
+		// müssen und dass root auf null initialisiert wird.
+		if(root == null) {root = new Node();
+		root.count=1;
+		root.keys[0] = key;
+		root.isLeaf = true;
+		return;}
+		
+		//find insert node;
+		Node current = root;
+		while(true) {
+			int i = 0;
+			for(i = 0; i <= current.count-1;i++) {
+				if(current.keys[i] > key) break;
+			}
+			if(current.isLeaf)break;
+			current = current.links[i];
+			
+		}
+		
+		//current = leaf to be inserted in
+		Node bro = null, child = null;
+		while(true) {
+			
+			//new root
+			if(current == null) {
+				root = new Node();
+				bro.parent= root;
+				child.parent = root;
+				root.keys[0]= key;
+				root.count++;
+				root.links[0] = child;
+				root.links[1] = bro;
+				if(bro.isLeaf==false)
+				{for(int i = 0; i <= bro.count;i++) {
+					bro.links[i].parent = bro;
+				}
+					}
+				return;
+			}
+			
+			//find pos to insert
+			int insertPos;
+			for(insertPos = 0; insertPos <= current.count-1;insertPos++) {
+				if(current.keys[insertPos] > key) break;
+			}
+			int median = current.keys[1];
+			
+			//verschiebe keys und links nach rechts
+			for(int i = current.keys.length-2; i >= insertPos; i--) {
+				current.keys[i+1] = current.keys[i];
+			}
+			for(int i = current.links.length-2; i > insertPos; i--) {
+				current.links[i+1] = current.links[i];
+			}
+			
+			//insert key and link
+			current.keys[insertPos] = key;
+			current.count++;
+			
+			if (bro != null) {
+				current.links[insertPos+1] = bro;
+				
+				//update parent for all nodes of bro to "bro"
+				if(bro.isLeaf == false) {
+					for(int i = 0; i <= bro.count;i++) {
+					bro.links[i].parent = bro;
+					}
+				}
+			}
+
+			
+			
+			
+			//check if node overflows
+			if(current.count <= 3) return;
+			
+			//node overflows
+			bro = new Node();bro.parent = current.parent;
+			
+			//extract median
+			int medianIndex = median > key ? 2 : 1;
+			key = median;
+			
+			//move keys to bro
+			current.keys[medianIndex] = 0; current.count--;
+			for(int i = medianIndex+1; i < current.keys.length;i++) {
+				bro.keys[i-(medianIndex+1)] = current.keys[i];
+				current.keys[i] = 0;
+				current.count--;bro.count++;
+				
+			}
+			
+			//move links to bro
+			for(int i = medianIndex+1; i < current.links.length; i++) {
+				bro.links[i-(medianIndex+1)] = current.links[i];
+				current.links[i] = null;
+			}
+			
+			if(bro.links[0] == null) {bro.isLeaf = true;}
+			child = current;
+			current = current.parent;
+			
+		}
+	}
 	public static void main(String[] args) {
 		Random rnd = new Random();
 		boolean[] set = new boolean[10000];
